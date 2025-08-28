c184e03c74f0aa8642128f562415868b59f6dc15
alexandretonarqui <alexandre.tonarqui@gmail.com>
2025-08-28 14:02:16 -0300

    JPA repository, dependency injection, database seeding

---
+++ course/src/main/java/com/aletonarqui/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aletonarqui.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aletonarqui.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
